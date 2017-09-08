2cf0805456281281d4ab43ec839656d99e81ef70
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2017-09-08 17:11:13 +0200

    :bear: show the word separator as a string

--- src/pwdgenerator_re_frame/core.cljs
+++ src/pwdgenerator_re_frame/core.cljs
@@ -92,7 +92,8 @@
                    :size 3
                    :maxLength 3
                    :value (:word_separator @s)
-                   :on-change #(swap! s assoc :word_separator (-> % .-target .-value))}]]
+                   :on-change #(swap! s assoc :word_separator (-> % .-target .-value))}]
+          " " (pr-str (:word_separator @s))]
          (for [[desc valid?] validations]
            (when (:focus? @s)
              [:div {:style {:color (when (:dirty? @s)

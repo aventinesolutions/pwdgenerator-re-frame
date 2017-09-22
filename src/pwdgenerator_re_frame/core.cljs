e467cd47f9a4eab9e936e42f367faf592cffcfc1
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2017-09-22 17:42:33 +0200

    :bear: set all defaults and finish form

--- src/pwdgenerator_re_frame/core.cljs
+++ src/pwdgenerator_re_frame/core.cljs
@@ -7,6 +7,17 @@
  (fn [_ _]
    {}))
 
+(def defaults {:no_words 5
+               :uppercase "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+               :no_uppercase_alpha 5
+               :lowercase "abcdefghijklmnopqrstuvwxyz"
+               :no_lowercase_alpha 5
+               :symbols "!@#$&*.:?+="
+               :no_symbols 1
+               :numerics "0123456789"
+               :no_numerics 3
+               :word_separator " "})
+
 (def password-validations
   [["At least 12 characters"
     (fn [s]
@@ -20,13 +31,7 @@
           (>= 0.5)))]])
 
 (defn pwdgenerator [pw]
-  (let [s (reagent/atom {:value pw
-                         :no_words 5
-                         :no_uppercase_alpha 5
-                         :no_lowercase_alpha 5
-                         :no_symbols 1
-                         :no_numerics 3
-                         :word_separator " "})]
+  (let [s (reagent/atom (merge defaults {:value pw}))]
     (fn []
       (let [validations (for [[desc f] password-validations]
                           [desc (f (:value @s))])
@@ -59,12 +64,26 @@
                    :value (:no_words @s)
                    :on-change #(swap! s assoc :no_words (-> % .-target .-value))}]]
          [:br]
+         [:label "Upper Case Alpha Character Set "
+          [:input {:type :text
+                   :size 35
+                   :maxLength 26
+                   :value (:uppercase @s)
+                   :on-change #(swap! s assoc :uppercase (-> % .-target .-value))}]]
+         [:br]
          [:label "Number of Upper Case Alpha Characters "
           [:input {:type :text
                    :size 3
                    :maxLength 3
                    :value (:no_uppercase_alpha @s)
                    :on-change #(swap! s assoc :no_uppercase_alpha (-> % .-target .-value))}]]
+         [:br]
+         [:label "Lower Case Alpha Character Set "
+          [:input {:type :text
+                   :size 35
+                   :maxLength 26
+                   :value (:lowercase @s)
+                   :on-change #(swap! s assoc :lowercase (-> % .-target .-value))}]]
          [:br]
          [:label "Number of Lower Case Alpha Characters "
           [:input {:type :text
@@ -73,12 +92,26 @@
                    :value (:no_lowercase_alpha @s)
                    :on-change #(swap! s assoc :no_lowercase_alpha (-> % .-target .-value))}]]
          [:br]
+         [:label "Numeric Character Set "
+          [:input {:type :text
+                   :size 10
+                   :maxLength 10
+                   :value (:numerics @s)
+                   :on-change #(swap! s assoc :numerics (-> % .-target .-value))}]]
+         [:br]
          [:label "Number of Numeric Characters "
           [:input {:type :text
                    :size 3
                    :maxLength 3
                    :value (:no_numerics @s)
                    :on-change #(swap! s assoc :no_numerics (-> % .-target .-value))}]]
+         [:br]
+         [:label "Symbol Character Set "
+          [:input {:type :text
+                   :size 15
+                   :maxLength 15
+                   :value (:symbols @s)
+                   :on-change #(swap! s assoc :symbols (-> % .-target .-value))}]]
          [:br]
          [:label "Number of Symbol Characters "
           [:input {:type :text
